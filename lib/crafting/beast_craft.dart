6fe6f3fcb46567e4775c04cab68dd169e3120064
Rikard Petre <rikkypikki@hotmail.com>
2019-08-12 20:18:49 +0200

    Added beast remove prefix, add suffix and vice versa

--- lib/crafting/beast_craft.dart
+++ lib/crafting/beast_craft.dart
@@ -1,4 +1,5 @@
 import 'package:poe_clicker/crafting/item/magic_item.dart';
+import 'package:poe_clicker/crafting/item/rare_item.dart';
 
 import 'item/item.dart';
 import 'mod.dart';
@@ -13,9 +14,11 @@ class BeastCraftCost {
 abstract class BeastCraft {
   static List<BeastCraft> allCrafts = [
     BeastMagicImprint(),
-    BeastMagicRestoreImprint()
+    BeastMagicRestoreImprint(),
+    BeastAddPrefixRemoveSuffix(),
+    BeastAddSuffixRemovePrefix(),
   ];
-  static List<BeastCraft> getBeastCraftsForItem(Item item){
+  static List<BeastCraft> getBeastCraftsForItem(Item item) {
     return allCrafts.where((craft) => craft.canDoCraft(item)).toList(growable: false);
   }
 
@@ -31,7 +34,7 @@ class BeastMagicImprint extends BeastCraft {
   String get displayName => "Create an Imprint";
   String get subheader => "Of a Magic Item";
   BeastCraftCost get cost => BeastCraftCost("ManBearPig", 1);
-  Item doCraft(Item item){
+  Item doCraft(Item item) {
     List<Mod> prefixes = List.generate(item.prefixes.length, (index) => Mod.copy(item.prefixes[index]));
     List<Mod> suffixes = List.generate(item.suffixes.length, (index) => Mod.copy(item.suffixes[index]));
     Item imprint = MagicItem.fromItem(item, prefixes, suffixes);
@@ -39,7 +42,7 @@ class BeastMagicImprint extends BeastCraft {
     return item;
   }
 
-  bool canDoCraft(Item item){
+  bool canDoCraft(Item item) {
     return item is MagicItem;
   }
 }
@@ -47,12 +50,48 @@ class BeastMagicImprint extends BeastCraft {
 class BeastMagicRestoreImprint extends BeastCraft {
   String get displayName => "Restore Imprinted Item";
   String get subheader => "Of the Current Item";
-  
+
   Item doCraft(Item item) {
     return item.imprint;
   }
 
-  bool canDoCraft(Item item){
+  bool canDoCraft(Item item) {
     return item.imprint != null;
+  }
+}
+
+class BeastAddPrefixRemoveSuffix extends BeastCraft {
+  String get displayName => "Add a Prefix, Remove a Random SuffIx";
+
+  Item doCraft(Item item) {
+    item.suffixes.removeAt(item.rng.nextInt(item.suffixes.length));
+    item.addPrefix();
+    return item;
+  }
+
+  bool canDoCraft(Item item) {
+    return (item is RareItem) &&
+        !item.hasMaxPrefixes() &&
+        item.suffixes.isNotEmpty &&
+        !item.hasCannotChangePrefixes() &&
+        !item.hasCannotChangeSuffixes();
+  }
+}
+
+class BeastAddSuffixRemovePrefix extends BeastCraft {
+  String get displayName => "Add a SuffIx, Remove a Random Prefix";
+
+  Item doCraft(Item item) {
+    item.prefixes.removeAt(item.rng.nextInt(item.prefixes.length));
+    item.addSuffix();
+    return item;
+  }
+
+  bool canDoCraft(Item item) {
+    return (item is RareItem) &&
+        !item.hasMaxSuffixes() &&
+        item.prefixes.isNotEmpty &&
+        !item.hasCannotChangePrefixes() &&
+        !item.hasCannotChangeSuffixes();
   }
 }
